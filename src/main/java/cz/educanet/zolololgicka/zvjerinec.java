2bffc71928a24878ae9a6a3ed653eec7c3e938f2
Chesters-CZ <bezouskavit@gmail.com>
2021-01-15 11:21:55 +0100

    now ekšuely works (novinka új!)

--- src/main/java/cz/educanet/zolololgicka/zvjerinec.java
+++ src/main/java/cz/educanet/zolololgicka/zvjerinec.java
@@ -6,23 +6,23 @@ import java.util.Arrays;
 
 @Path("zvjer")
 public class zvjerinec {
-    public static String[] enimelz;
+    public static String[] enimelz = new String[1024];
 
     @PUT
-    public Response addzvjer(@FormParam("animal") String name, @FormParam("id") int pos){
+    public Response addzvjer(@QueryParam("animal") String name, @QueryParam("id") int pos){
         if (enimelz[pos] != null) return Response.status(400).build();
         enimelz[pos] = name;
         return Response.ok().build();
     }
 
     @DELETE
-    public Response remzvjer(@FormParam("id") int pos){
+    public Response remzvjer(@QueryParam("id") int pos){
         enimelz[pos] = null;
         return Response.ok().build();
     }
 
     @GET
-    public Response getzvjer(@DefaultValue("-2147483648") @FormParam("id") int pos){
+    public Response getzvjer(@DefaultValue("-2147483648") @QueryParam("id") int pos){
         if (pos == -2147483648) return Response.ok(Arrays.toString(enimelz)).build();
         return Response.ok(enimelz[pos]).build();
     }
